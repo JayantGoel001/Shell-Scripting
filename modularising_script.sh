19acc516f9ec04f1a4d6a800c7d203a4552a0889
username <54479676+CormacKrum@users.noreply.github.com>
2020-11-17 01:48:42 +0530

    Initial Commit

---
+++ modularising_script.sh
@@ -0,0 +1,7 @@
+function mylog(){
+	echo -e "$1" | tee -a $LOGFILE
+}
+
+function test_string(){
+	echo "$1 $2"
+}
---
+++ modularising_script2.sh
@@ -0,0 +1,4 @@
+source ./modularising_script.sh
+LOGFILE=myout.txt
+mylog "Testing modularising script"
+test_string "Hello " "User"
